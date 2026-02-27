4295abebdb03ee992b7218d229eb3aa551f6a1f7
Megan Matthews <ST10504206@myemeris.edu.za>
2026-02-27 11:37:39 +0200

    Initial Commit

---
+++ src/calculatorgithub/CalculatorGithub.java
@@ -0,0 +1,12 @@
+package calculatorgithub;
+
+public class CalculatorGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
