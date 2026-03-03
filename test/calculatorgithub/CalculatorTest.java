4d53e032585133f94e366a980816e03bc6c95223
Megan Matthews <ST10504206@myemeris.edu.za>
2026-03-03 09:26:17 +0200

    Testing Code
    Made sure the testing was working

---
+++ test/calculatorgithub/CalculatorTest.java
@@ -0,0 +1,22 @@
+package calculatorgithub;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    
+    @Test
+    public void addsTwoNumbers(){
+        Calculator calculator = new Calculator();
+        assertEquals("1 + 1 = 2", 2, calculator.add(1,1));
+    }
+    
+    @Test
+    public void subtractTwoNumbers(){
+        Calculator calculator = new Calculator();
+        assertEquals("2 - 1 = 1", 1, calculator.subtract(2,1));
+    }
+    
+    
+    
+}
